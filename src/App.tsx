84e96bf14577a1486e46726ba745c8a7a46a6892
Rhuan <rhuanlopes536@gmail.com>
2023-02-16 20:13:46 -0300

    initing repo

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl font-bold">Template feito pelo Rhuan!</h1>
+    </div>
+  );
+}
+
+export default App;
