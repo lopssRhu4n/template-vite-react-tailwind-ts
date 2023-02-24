18ab090bd9fdced7eddc70a796eabdd7bb3b7b9d
Rhuan <rhuanlopes536@gmail.com>
2023-02-23 21:22:55 -0300

    Adding path aliases to project (WIP - ESLint)

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
+/* eslint-disable import/no-unresolved */
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { MainPage } from './pages/MainPage';
+import { MainPage } from '@/pages/MainPage';
 import { AboutPage } from './pages/AboutPage';
 import { Provider } from 'react-redux';
 import { store } from './stores';
 
 function App() {
-  return (
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route element={<MainPage />} path="/" />
-          <Route element={<AboutPage />} path="/about" />
-        </Routes>
-      </Router>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <Router>
+                <Routes>
+                    <Route element={<MainPage />} path="/" />
+                    <Route element={<AboutPage />} path="/about" />
+                </Routes>
+            </Router>
+        </Provider>
+    );
 }
 
 export default App;
